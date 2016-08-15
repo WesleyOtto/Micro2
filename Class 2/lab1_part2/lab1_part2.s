3f7c56d871bcf850f98743eb0d01f112806d8f7c
BRUNO <brunoovedoveto@gmail.com>
2016-08-15 20:39:51 -0300

    Fibonacci exercise

--- Class 2/lab1_part2/lab1_part2.s
+++ Class 2/lab1_part2/lab1_part2.s
@@ -1,33 +1,34 @@
-/* Program that calculates the fibonacci sequence	    */
-
-.equ LIST, 0xFFC		  /* Starting address of the list		    */
+.equ MEM, 0x1000
+.equ N, 15
 
 .global _start
 _start:
-	movia	r4, LIST	  /* r4 points to the start of the list	    */
-	addi	r5, r4, 4	  /* r5 is the adress to the first element of the sequence    */
+	movia r5, MEM	
+	movia r8, N
 	
-	add	r6, r0, r0	  /* first element is 0		    */
+	add r6, r0, r0	 
 	stw r6, 0(r5)
+	addi r5, r5, 4
 
- 	addi r6, r0, 1	  /* second element is 1		    */
-	stw r6, 4(r5)
-
-	addi	r5, r5, 8   /* Counter of the vector */
-	addi	r8, r4, r0  /* Counter of number of elements */
-	subi    r8, r8, 2
+ 	addi r7, r0, 1
+	stw r7, 0(r5)
+	addi r5, r5, 4
+ 
+	subi r8, r8, 2
 
 	LOOP:
-	     subi	r8, r8, 1
-	     beq	r8, r0, DONE  /* Finished if r8 is equal to 0		    */
-         
-         /* TODO */
-
-
-.org	0xFFC
-N:
-.word	8			  /* Space for the test number	    */
-RESULT:
-.word 			  /* Start of result memories adress    */
-
+	     subi r8, r8, 1
+	     beq r8, r0, DONE  
+	     
+	     add r9, r7, r0
+	     add r7, r6, r7
+	     add r6, r9, r0
+	     
+	     stw r7, 0(r5)
+	     addi r5, r5, 4
+	     
+	     br LOOP
+	          
+	DONE:
+	
 .end

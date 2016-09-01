2c82c8dc09845ccaf672b8dacfa5682105f59802
vinacovre <viniassis@me.com>
2016-09-01 15:02:24 -0300

    some additional comments

--- Class 5/lab4_part2/lab4_part2.s
+++ Class 5/lab4_part2/lab4_part2.s
@@ -1,12 +1,14 @@
 .equ LED_BASE_ADDRESS, 0x10000010
 .equ SWITCH_BASE_ADDRESS, 0x10000040
-.equ PUSHBUTTON_ADDRESS, 0x1000005C
+.equ PUSHBUTTON_ADDRESS, 0x1000005C # Third push button from left to right
 .equ MASK, 0x02 					# 00000010
+/* we use this maske because we need push button 3 active...
+PUSH BUTTONS (PBs): 0 0 1 0 == PB1 PB2 PB3 PB4 (we need pb3 active) */
 
 .global _start
 _start:
 
-	movia r4, LED_BASE_ADDRESS  
+	movia r4, LED_BASE_ADDRESS
 	movia r5, SWITCH_BASE_ADDRESS
 	movia r8, PUSHBUTTON_ADDRESS
 	movia r10, MASK

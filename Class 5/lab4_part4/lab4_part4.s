66a5f7909563dc154b77ff37829fbe28c2803c1c
Vinicius Covre <viniassis@me.com>
2016-09-21 21:41:43 -0300

    final version

--- Class 5/lab4_part4/lab4_part4.s
+++ Class 5/lab4_part4/lab4_part4.s
@@ -1,6 +1,7 @@
 .equ DISPLAY_BASE_ADDRESS, 0x10000020
 .equ SWITCH_BASE_ADDRESS, 0x10000040
-.equ PUSHBUTTON_ADDRESS, 0x1000005C
+.equ PUSHBUTTON_BASE_ADDRESS, 0x10000050
+.equ LED_BASE_ADDRESS, 0x10000010
 .equ MASK, 0x02 					# 00000010 Button flag
 .equ MASK2, 0xf						# 00001111 digit separator
 
@@ -12,14 +13,17 @@
 HARDWARE_EXCEPTION:					# Standard program
 
 	subi ea, ea, 4
-	ldwio r9, 0(r8)					# Word to buttons flags
+	ldwio r9, 12(r8)					# Word to buttons flags
 	and r9, r9, r10
 	bne r9, r10, END_HANDLER		# When the button is pushed, the loop ends
+
+	stwio r0, 12(r8)				# seta 0 no edge capture do pushbutton para poder ser pressionado novamente
 
 	ldwio r7, 0(r5)					# First value of the switch
 	add r6, r6, r7					# Accumulating value
 
 	add r14, r6, r0					# Save the r6
+	stwio r6, 0(r18)				# set green leds
 
 	add r15, r0, r0					# Number to shift
 	add r16, r0, r0					# Auxiliar to use OR
@@ -42,7 +46,7 @@ LOOP:
 
 	add r6, r14, r0					# Return the r6 value
 
-	stwio r0, 0(r8)					# Set to 0 the value of the button
+	#stwio r0, 0(r8)					# Set to 0 the value of the button
 
 OTHER_EXCEPTIONS:
 
@@ -51,17 +55,22 @@ END_HANDLER:
 
 .global _start
 _start:
+
+	movia r4, DISPLAY_BASE_ADDRESS
 	movia r5, SWITCH_BASE_ADDRESS
-	movia r8, PUSHBUTTON_ADDRESS
+	movia r8, PUSHBUTTON_BASE_ADDRESS
 	movia r10, MASK
 	movia r11, MAP
 	movia r12, MASK2
+	movia r18, LED_BASE_ADDRESS
 
 	xor r6, r6, r6 					# Putting initial value zero at the Accumulator
 
-	rdctl ienable, 1
-	rdctl irqn, 1
-
+	stwio r10, 8(r8)				# habilita a instrução de interrupção no botao 2
+	wrctl ienable, r10				# enable control registrer to puhsbutton 2
+	movi r10, 1						# Set second pushbutton to be valid
+	wrctl status, r10				# status == CTL0 # set PIE to 1
+	movia r10, MASK
 
 END:
 	br END

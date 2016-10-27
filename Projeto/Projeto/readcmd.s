4c84f7d3fd5c64f3c068ccab7d4097909633af37
Bancada 2 <bancada2@lab.micro2.2015>
2016-10-27 00:05:59 -0200

    Which command was entered

--- Projeto/Projeto/readcmd.s
+++ Projeto/Projeto/readcmd.s
@@ -32,15 +32,71 @@ WRITE:
 	movia r4, ENTER
 	beq r10, r4, EXECUTE			# If ENTER is hit, execute COMMAND
 
-	stw r10, 0(r8)					# Keep command value at memory
+	stw r10, 0(r8)					# Keep command value on memory
 	addi r8, r8, 4
 
 	br READ
 
 EXECUTE:
-	
+	movia r8, LASTCMD
+	ldw r9, 0(r8)
+	subi r9, r9, 0x30
+	ldw r10, 4(r8)
+	subi r10, r10, 0x30
+
+	# Multiply r9 by 10 and add to r10
+	slli r11, r9, 3
+	slli r12, r9, 1
+	add r9, r11, r12
+	add r9, r9, r10
+
+	# Test which command user entered
+	addi r10, r0, 00
+	beq r9, r10, LED_ON
+	addi r10, r0, 01
+	beq r9, r10, LED_OFF
+	addi r10, r0, 10
+	beq r9, r10, TRIANG_NUM
+	addi r10, r0, 20
+	beq r9, r10, DISPLAY_MSG
+	addi r10, r0, 21
+	beq r9, r10, CANCEL_ROT
 
 	br BEGIN
+
+LED_ON:
+
+
+	br BEGIN
+
+
+LED_OFF:
+
+
+
+	br BEGIN
+
+
+TRIANG_NUM:
+
+
+
+	br BEGIN
+
+
+DISPLAY_MSG:
+
+
+
+	br BEGIN
+
+
+CANCEL_ROT:
+
+
+
+	br 	BEGIN
+
 
 .org 0x100
 LASTCMD:

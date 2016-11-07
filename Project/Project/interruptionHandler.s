1fb2bcb1c5bd2aaa2f9b6ac56e11b59082acb84c
vinacovre <viniassis@me.com>
2016-11-07 20:15:20 -0200

    standardizing code

--- Project/Project/interruptionHandler.s
+++ Project/Project/interruptionHandler.s
@@ -1,30 +1,32 @@
 .equ MASK_CPU_INTERRUP,		0x1
-.equ MASK_START, 			0x7
+.equ MASK_START, 					0x7
 .equ TIMER_BASEADRESS,		0x10002000
-.equ TIME_COUNTERH, 		0x017d 				#25 MILION high (before change 0x017d)
-.equ TIME_COUNTERL, 		0x7840  			#25 MILION LOW
+.equ TIME_COUNTERH, 			0x017d 				# 25 MILION high (before change 0x017d)
+.equ TIME_COUNTERL, 			0x7840  			# 25 MILION LOW
 .equ REDLED_BASEADDRESS,	0x10000000
 
 .org 0x20
+
 /*********************************************************/
-/**********************INTERRUP****************************/
+/**********************INTERRUP***************************/
 /*********************************************************/
 
- /* Exception Handler */
-
+/* Exception Handler */
 	rdctl et, ipending
 	beq et, r0, OTHER_EXCEPTIONS
 
-HARDWARE_EXCEPTION:					# Standard program
+HARDWARE_EXCEPTION:						# Standardized code
 
 	subi ea, ea, 4
 	movia r16, REDLED_BASEADDRESS
 
-	andi r13, et, 0b1 				#interval timer is interrupt level 0
+	andi r13, et, 0b1 					# interval timer is interrupt level 0
 	beq r13, r0, END_HANDLER
 
 	movia r14, TIMER_BASEADRESS
-	sthio r0, 0(r14) 				#clear Time Out ( clear the interrupt )
+	sthio r0, 0(r14) 						# clear Time Out ( clear the interrupt )
+
+/********************** LED FLASH **********************/
 
 	ON:
 		bne	r15, r0, OFF
@@ -36,30 +38,28 @@ HARDWARE_EXCEPTION:					# Standard program
 		stwio r0, 0(r16)
 		add r15, r0, r0
 
-	OTHER_EXCEPTIONS:
-	END_HANDLER:
-		eret
+OTHER_EXCEPTIONS:
+END_HANDLER:
+	eret
 
 .global LEDFLASH
 LEDFLASH:
 
 /*********************************************************/
-/**********************PRÓLOGO****************************/
+/**********************PROLOGUE***************************/
 /*********************************************************/
 
-/* Adjust the stack pointer */
-	addi sp, sp, -8	/* make a 8-byte frame */
+# Adjust the stack pointer
+	addi sp, sp, -8									# make a 8-byte frame
 
-/* Store registers to the frame */
-	stw ra, 4(sp) 	/* store the return address */
-	stw fp, 0(sp) 	/* store the frame pointer*/
+# Store registers to the frame
+	stw ra, 4(sp)										# store the return address
+	stw fp, 0(sp) 									# store the frame pointer
 
-/* Set the new frame pointer */
+# Set the new frame pointer
 	addi fp, sp, 0
 
-/*********************************************************/
 /**********************SET INTERRUPTION*******************/
-/*********************************************************/
 
 movia r9,  MASK_CPU_INTERRUP
 movia r12, TIMER_BASEADRESS
@@ -67,26 +67,24 @@ movia r13, TIME_COUNTERH
 movia r17, TIME_COUNTERL
 movia r14, MASK_START
 
-add r15, r0, r0						 #Flag to LED ON or LED OFF
+add r15, r0, r0						 			# Flag to LED ON or LED OFF
 
 ######****** Start interval timer, enable its interrupts ******######
-sthio r17, 8(r12)  			# Set to low value
-sthio r13, 12(r12)  		# Set to high Value
-sthio r14, 4(r12)			# Set, START, CONT, E ITO = 1
+sthio r17, 8(r12)  							# Set to low value
+sthio r13, 12(r12)  						# Set to high Value
+sthio r14, 4(r12)								# Set, START, CONT, E ITO = 1
 
 ######******enable Nios II processor interrupts******######
-wrctl ienable, r9 		  #Set IRQ bit 0
-wrctl status, r9 		  #turn on Nios II interrupt processing ( SET PIE = 1 )
+wrctl ienable, r9 		  				# Set IRQ bit 0
+wrctl status, r9 		  					# turn on Nios II interrupt processing ( SET PIE = 1 )
 
 /********************************************************/
-/*********************EPILOGO****************************/
+/*********************EPILOGUE***************************/
 /********************************************************/
 
- /*Restore ra, fp, sp, and registers */
-
+# Restore ra, fp, sp, and registers
 	mov sp, fp
 	ldw ra, 4(sp)
 	ldw fp, 0(sp)
 	addi sp, sp, 0
-/* Return from subroutine */
-ret
+ret															# Return from subroutine
--- Project/Project/project.s
+++ Project/Project/project.s
@@ -1,28 +1,26 @@
-.equ MASK_RVALID, 	0x00008000	
-.equ MASK_DATA, 	0x000000FF
+.equ MASK_RVALID,		0x00008000
+.equ MASK_DATA, 		0x000000FF
 .equ MASK_WSPACE, 	0xFFFF0000
-.equ KEYBOARD, 		0x10001000
-.equ ENTER,			0x0000000A
-.equ BACKSPACE,		0x00000008
-.equ STACK, 		0x00002000
+.equ KEYBOARD, 			0x10001000
+.equ ENTER,					0x0000000A
+.equ BACKSPACE,			0x00000008
+.equ STACK, 				0x00002000
 
 .global _start
 _start:
 
-/*********************************************************/
-/**********************PRINT******************************/
-/*********************************************************/
+/********************** PRINT **********************/
 
-	movia sp, STACK 	/* Set stack registers and    */
-    mov fp, sp      	/* frame pointer.             */
-	call PRINTF			/* Call Function PRINTF		  */
+	movia sp, STACK 		# Set stack registers and
+  mov fp, sp	      	# frame pointer.
+	call PRINTF					# Call Function PRINTF
 
-	movia sp, STACK     /* Set stack registers and   					*/
-    mov fp, sp         	/* frame pointer.             				    */
-    call LEDFLASH	    /* Call Function LEDFLASH who sets INTERRUPTION */
+	movia sp, STACK     # Set stack registers and
+  mov fp, sp         	# frame pointer.
+  call LEDFLASH	    	#	Call Function LEDFLASH who sets INTERRUPTION
 
 BEGIN:
-	movia r8, LASTCMD				# After ENTER rewrite these addresses
+	movia r8, LASTCMD		# After ENTER rewrite these addresses
 
 READ:
 	movia r2, MASK_WSPACE
@@ -30,25 +28,25 @@ READ:
 	movia r4, MASK_DATA
 	movia r5, KEYBOARD
 
-	ldwio r9, 0(r5)					# R9 <- JTAG UART
-	and r10, r9, r3					# Verify availability [RVALID]
-	beq r10, r0, READ 				# if not avaiable, wait... 
+	ldwio r9, 0(r5)				# R9 <- JTAG UART
+	and r10, r9, r3				# Verify availability [RVALID]
+	beq r10, r0, READ 		# if not avaiable, wait...
 
-	and r10, r9, r4					# Get data from input 
+	and r10, r9, r4				# Get data from input
 
 WRITE:
-	ldwio r6, 4(r5)					# Read control register
-	and r3, r2, r6					# Verify space availability [WSPACE]
-	beq r0, r3, WRITE				# While there's no space, wait...
+	ldwio r6, 4(r5)				# Read control register
+	and r3, r2, r6				# Verify space availability [WSPACE]
+	beq r0, r3, WRITE			# While there's no space, wait...
 
-	stwio r10, 0(r5)				# Print char on the terminal (using Data Register)
+	stwio r10, 0(r5)			# Print char on the terminal (using Data Register)
 
 	movia r4, ENTER
-	beq r10, r4, EXECUTE			# If ENTER is hit, execute COMMAND
+	beq r10, r4, EXECUTE	# If ENTER is hit, execute COMMAND
 	movia r4, BACKSPACE
-	beq r10, r4, ERASE				# If BACKSPACE is hit, erase last char from memory
+	beq r10, r4, ERASE		# If BACKSPACE is hit, erase last char from memory
 
-	stw r10, 0(r8)					# Keep command value on memory
+	stw r10, 0(r8)				# Keep command value on memory
 	addi r8, r8, 4
 
 	br READ
@@ -84,68 +82,61 @@ EXECUTE:
 
 	br BEGIN
 
-LED_ON:
-  	# Get LED number (0x30 is the ASCII base value)
-  	ldw r9, 8(r8)
-  	subi r9, r9, 0x30
-  	ldw r10, 12(r8)
-  	subi r10, r10, 0x30
-  
-  	# Multiply r9 by 10 and add to r10
-  	slli r11, r9, 3
-  	slli r12, r9, 1
-  	add r9, r11, r12
-  	add r9, r9, r10
-  
-  	# Set bit to turn ON the LED
-  	addi r10, r0, 1
-  	sll r10, r10, r9
-  	or r7, r7, r10
-  
-  	br BEGIN
-  
-  
-LED_OFF:
-  	# Get LED number (0x30 is the ASCII base value)
-  	ldw r9, 8(r8)
-  	subi r9, r9, 0x30
-  	ldw r10, 12(r8)
-  	subi r10, r10, 0x30
-  
-  	# Multiply r9 by 10 and add to r10
-  	slli r11, r9, 3
-  	slli r12, r9, 1
-  	add r9, r11, r12
-  	add r9, r9, r10
-  
-  	# Unset bit to turn OFF the LED
-  	addi r10, r0, 1
-  	sll r10, r10, r9
-  	nor r10, r10, r10
-  	and r7, r7, r10
-  
-  	br BEGIN
+/********************** FUNCTIONS **********************/
 
+LED_ON:
+	# Get LED number (0x30 is the ASCII base value)
+	ldw r9, 8(r8)
+	subi r9, r9, 0x30
+	ldw r10, 12(r8)
+	subi r10, r10, 0x30
+
+	# Multiply r9 by 10 and add to r10
+	slli r11, r9, 3
+	slli r12, r9, 1
+	add r9, r11, r12
+	add r9, r9, r10
+
+	# Set bit to turn ON the LED
+	addi r10, r0, 1
+	sll r10, r10, r9
+	or r7, r7, r10
+
+	br BEGIN
+
+LED_OFF:
+	# Get LED number (0x30 is the ASCII base value)
+	ldw r9, 8(r8)
+	subi r9, r9, 0x30
+	ldw r10, 12(r8)
+	subi r10, r10, 0x30
+
+	# Multiply r9 by 10 and add to r10
+	slli r11, r9, 3
+	slli r12, r9, 1
+	add r9, r11, r12
+	add r9, r9, r10
+
+	# Unset bit to turn OFF the LED
+	addi r10, r0, 1
+	sll r10, r10, r9
+	nor r10, r10, r10
+	and r7, r7, r10
+
+	br BEGIN
 
 TRIANG_NUM:
 
-
-
 	br BEGIN
-
 
 DISPLAY_MSG:
 
-
-
 	br BEGIN
-
 
 CANCEL_ROT:
 
-
-
 	br 	BEGIN
 
+/********************** COMMAND BUFFER **********************/
 .org 0x2500
 LASTCMD:

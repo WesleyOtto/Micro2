5494d271b562a7005e47eb7f359f637f9625b4e2
vinacovre <viniassis@me.com>
2016-11-30 12:57:45 -0200

    finishing indentation

--- Project/Project/print.s
+++ Project/Project/print.s
@@ -8,12 +8,14 @@ PRINTF:
 /*********************************************************/
 
 # Adjust the stack pointer
-	addi sp, sp, -8						# make a 8-byte frame
+	addi 	sp, sp, -8					# make a 8-byte frame
+
 # Store registers to the frame
-	stw ra, 4(sp) 						# store the return address
-	stw fp, 0(sp) 						# store the frame pointer
+	stw 	ra, 4(sp) 					# store the return address
+	stw 	fp, 0(sp) 					# store the frame pointer
+
 # Set the new frame pointer
-	addi fp, sp, 0
+	addi 	fp, sp, 0
 
 /**********************PRINT******************************/
 
@@ -22,33 +24,33 @@ PRINTF:
 	movia r13, MSG
 	movia r14, MSG_SIZE
 
-	ldw r14, 0(r14)						# Get Message Size
+	ldw 	r14, 0(r14)					# Get Message Size
 
 PRINT:
-	ldw r10, 0(r13)	 					# Load letter from memory
+	ldw 	r10, 0(r13)	 				# Load letter from memory
 
 SPACE_LOOP:
 
 	ldwio r11, 4(r8) 					# Read control register
-	and r12, r9, r11	 				# Verify space availability [WSPACE]
-	beq r0, r12, SPACE_LOOP		        # While there's no space, wait...
+	and 	r12, r9, r11	 			# Verify space availability [WSPACE]
+	beq 	r0, r12, SPACE_LOOP	# While there's no space, wait...
 
 	stwio r10, 0(r8) 					# Print on the terminal (using Data Register)
 
-	addi r13, r13, 4					# Next letter
-	subi r14, r14, 1					# Counter to word size
-	bne r14, r0, PRINT
+	addi 	r13, r13, 4					# Next letter
+	subi 	r14, r14, 1					# Counter to word size
+	bne 	r14, r0, PRINT
 
 /*********************************************************/
 /**********************EPILOGUE***************************/
 /*********************************************************/
 
 # Restore ra, fp, sp, and registers
-	mov sp, fp
-	ldw ra, 4(sp)
-	ldw fp, 0(sp)
-	addi sp, sp, 0
-	ret												# Return from subroutine
+	mov 	sp, fp							# sp points to fp
+	ldw 	ra, 4(sp)						# loads back ra
+	ldw 	fp, 0(sp)						# loads back fp
+	addi 	sp, sp, 0						# sp for the empty stack
+	ret
 
 MSG_SIZE:
 .word 21
